9b65c7a12c003f29ed8279881e5c4b8af0d9d7e0
marko-13 <marko07031997@gmail.com>
2021-03-09 10:37:21 +0100

    readme update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # siap-lyrics-to-genre
 SIAP project
+
+## On download run python scripts in following order:
+### 1. genre_cleaning.py
+### 2. (optional, run only if you do not have lyrics dataset) run_scraping.py
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import data_processing.lyrics_scraping as scraping
 import lyricsgenius as lg
 
 if __name__ == "__main__":
-    # song_url = scraping.request_song_url(artist_name='Daft Punk',
-    #                                      song_name='One more time',
-    #                                      song_cap=1,
-    #                                      print_response=True)
     genius = lg.Genius('Qc5VC8btnP5JS4GLgoR6j1SnGWqJafxshVymsRHNff_Gdmk-CO5sJuefbtFCfTlO',
                        skip_non_songs=True,
                        excluded_terms=["(Remix)", "(Live)"],
